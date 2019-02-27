64a1f81078658458c867cd45f201ddfa38740864
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-27 18:57:55 +0000

    Automatically backed up by Learn

---
+++ app/models/team.rb
@@ -0,0 +1,20 @@
+class Team 
+  attr_accessor :name, :power, :bio
+
+  @@all = []
+
+  def initialize(details)
+    @name = details[:name]
+    @power = details[:power]
+    @bio = details[:bio]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all = []
+  end
+end
